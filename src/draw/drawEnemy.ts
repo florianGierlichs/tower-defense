d704b39b1a86e102565bc2f31eb23373286fb8b8
florianGierlichs <gierlichs@gmx.de>
2022-11-14 22:55:30 +0100

    Cleanup

--- src/draw/drawEnemy.ts
+++
@@ -1,6 +0,0 @@
-export const drawEnemy = (context: CanvasRenderingContext2D, enemy: any) => {
-  context.beginPath();
-  context.arc(enemy.x, enemy.y, 10, 0, Math.PI * 2);
-  context.fillStyle = enemy.color;
-  context.fill();
-};
--- src/draw/drawTower.ts
+++
@@ -1,7 +0,0 @@
-export const drawTower = (context: CanvasRenderingContext2D, tower: any) => {
-  context.beginPath();
-  context.lineWidth = 5;
-  context.fillStyle = tower.color;
-  context.fillRect(tower.x, tower.y, tower.width, tower.height);
-  context.stroke();
-};
