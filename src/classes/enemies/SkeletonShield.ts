8344354490aaee91bbfbbd320e16baeca7f5e9d9
Florian Gierlichs <florian.gierlichs@paymenttools.com>
2023-07-31 12:45:03 +0200

    Add SkeletonShield enemy

---
+++ src/classes/enemies/SkeletonShield.ts
@@ -0,0 +1,37 @@
+import { imageController } from "../../main";
+import { EnemyConfig } from "../../utils/types";
+import { Enemy } from "./Enemy";
+
+export class SkeletonShield extends Enemy {
+  static readonly config: EnemyConfig = {
+    name: "skeletonShield",
+    health: 3,
+    speed: 2,
+    imageScale: 0.8,
+    frameConfig: {
+      move: {
+        frames: 4,
+        animationIterationCircleTime: 500,
+        flipOffsetFrames: 0,
+        animationStartRight: {
+          sx: 0,
+          sy: 64,
+        },
+        animationStartLeft: {
+          sx: 192,
+          sy: 384,
+        },
+      },
+    },
+  };
+
+  constructor(id: string, x: number, y: number) {
+    super(
+      id,
+      x,
+      y,
+      imageController.getImage(SkeletonShield.config.name),
+      SkeletonShield.config
+    );
+  }
+}
---
+++ src/utils/getRandomFrameIteration.ts
@@ -0,0 +1,3 @@
+export const getRandomFrameIteration = (frames: number) => {
+  return Math.floor(Math.random() * (frames + 1));
+};
