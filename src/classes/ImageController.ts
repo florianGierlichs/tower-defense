6ea0d100ec5a87d9ff5dda27f17621ff7d9de24f
Florian Gierlichs <florian.gierlichs@paymenttools.com>
2023-08-02 15:19:04 +0200

    Rework and extend image controller

--- src/classes/ImageController.ts
+++ src/classes/ImageController.ts
@@ -1,46 +1,52 @@
 import { loadImage } from "../utils/loadImage";
-import arcaneArcherBp from "../assets/arcaneArcherMenu.png";
-import textures from "../assets/textures.png";
-import skeleton from "../assets/skeletonShield.png";
-import { TOWER_CONFIGS, TowerConfig } from "../data/towerConfig";
-import { EnemyNames } from "./Waves";
-
-// https://astrobob.itch.io/arcane-archer
-// https://cainos.itch.io/pixel-art-top-down-basic
-// https://luizmelo.itch.io/monsters-creatures-fantasy
+import { EnemyName } from "./Waves";
+import { TowerBpName, TowerName, TowerProjectileName } from "./Menu";
+import arcaneArcherImg from "../assets/arcaneArcher.png";
+import arcaneArcherProjectileImg from "../assets/arcaneArcherProjectile.png";
+import arcaneArcherBpImg from "../assets/arcaneArcherMenu.png";
+import texturesImg from "../assets/textures.png";
+import skeletonShieldImg from "../assets/skeletonShield.png";
 
 const texturesKey = "textures";
 
 export class ImageController {
-  arcaneArcherBpImage: HTMLImageElement | null = null;
-  textures: HTMLImageElement | null = null; // todo prio name clash with import names
-  skeleton: HTMLImageElement | null = null;
+  textures: HTMLImageElement | null = null;
+
+  // tower
+  arcaneArcher: HTMLImageElement | null = null;
+  arcaneArcherProjectile: HTMLImageElement | null = null;
+  arcaneArcherBp: HTMLImageElement | null = null;
+  //someTower: HTMLImageElement | null = null;
+
+  // enemy
+  skeletonShield: HTMLImageElement | null = null;
+  //someEnemy: HTMLImageElement | null = null;
 
   loadImages = async () => {
-    this.arcaneArcherBpImage = await loadImage(arcaneArcherBp);
-    this.textures = await loadImage(textures);
-    this.skeleton = await loadImage(skeleton);
+    this.textures = await loadImage(texturesImg);
+
+    // tower
+    this.arcaneArcher = await loadImage(arcaneArcherImg);
+    this.arcaneArcherProjectile = await loadImage(arcaneArcherProjectileImg);
+    this.arcaneArcherBp = await loadImage(arcaneArcherBpImg);
+
+    // enemy
+    this.skeletonShield = await loadImage(skeletonShieldImg);
   };
 
-  getImage = (key: TowerConfig | typeof texturesKey | EnemyNames) => {
-    // const a: typeof texturesKey = "textures";
-    // console.log("test should be textures: HTMLImageElement | null", this[a]); // TODO maybe instead of switch case use this[key]?
-    if (!this.arcaneArcherBpImage || !this.textures || !this.skeleton) {
+  getImage = (
+    key:
+      | typeof texturesKey
+      | TowerName
+      | TowerProjectileName
+      | TowerBpName
+      | EnemyName
+  ): HTMLImageElement => {
+    const image = this[key];
+    if (image === null) {
       throw new Error("Images not loaded yet");
     }
 
-    switch (key) {
-      case TOWER_CONFIGS.arcaneArcher:
-        return this.arcaneArcherBpImage;
-
-      case texturesKey:
-        return this.textures;
-
-      case "skeletonShield":
-        return this.skeleton;
-
-      default:
-        return this.arcaneArcherBpImage;
-    }
+    return image;
   };
 }
